599a01d8b825cddd152990724f39e3aa2cbd31f9
gboddin <gregory@siwhine.net>
2025-07-27 20:05:44 +0200

    Cleaned, added support for good old url list

--- HostScanner.go
+++ HostScanner.go
@@ -33,11 +33,11 @@ func (hs *HostScanner) Start() {
 	}
 }
 
-func (hs *HostScanner) testSoftwareFingerprint(event l9format.L9Event, rule DetectionRule) bool {
+func (hs *HostScanner) testSoftwareFingerprint(event l9format.L9Event) bool {
 	// Test if target matches the software fingerprint
-	resp, err := hs.HttpClient.Get(event.Url() + rule.FingerprintCheck.Uri)
+	resp, err := hs.HttpClient.Get(event.Url() + hs.Rule.FingerprintCheck.Uri)
 	if err != nil {
-		log.Printf("Error fingerprinting host %s with rule %s: %v", event.Url(), rule.Name, err)
+		log.Printf("Error fingerprinting host %s with rule %s: %v", event.Url(), hs.Rule.Name, err)
 		return false
 	}
 	defer resp.Body.Close()
@@ -46,29 +46,29 @@ func (hs *HostScanner) testSoftwareFingerprint(event l9format.L9Event, rule Dete
 		log.Printf("Error reading fingerprint body for host %s: %v", event.Url(), err)
 		return false
 	}
-	if !strings.Contains(string(body), rule.FingerprintCheck.ExpectedContent) {
+	if !strings.Contains(string(body), hs.Rule.FingerprintCheck.ExpectedContent) {
 		return false
 	}
 	_, _ = io.Copy(io.Discard, resp.Body)
-	log.Printf("Host %s matches %s fingerprint", event.Url(), rule.Name)
+	log.Printf("Host %s matches %s fingerprint", event.Url(), hs.Rule.Name)
 	return true
 }
 
-func (hs *HostScanner) getNormalStatusCodeForRule(event l9format.L9Event, rule DetectionRule) int {
+func (hs *HostScanner) getNormalStatusCodeForRule(event l9format.L9Event) int {
 	// Get expected status code for non-existent files using rule's test Uri
 	randNumber := rand.IntN(10000000)
-	resp, err := hs.HttpClient.Get(event.Url() + fmt.Sprintf(rule.NonExistentFileUri, randNumber))
+	resp, err := hs.HttpClient.Get(event.Url() + fmt.Sprintf(hs.Rule.NonExistentFileUri, randNumber))
 	if err != nil {
-		log.Printf("Error getting baseline status code for host %s with rule %s: %v", event.Url(), rule.Name, err)
+		log.Printf("Error getting baseline status code for host %s with rule %s: %v", event.Url(), hs.Rule.Name, err)
 		return 0
 	}
 	defer resp.Body.Close()
 	_, _ = io.Copy(io.Discard, resp.Body)
-	log.Printf("Baseline status code for %s (%s): %d", event.Url(), rule.Name, resp.StatusCode)
+	log.Printf("Baseline status code for %s (%s): %d", event.Url(), hs.Rule.Name, resp.StatusCode)
 	return resp.StatusCode
 }
 
-func (hs *HostScanner) scanWithRule(event l9format.L9Event, uri string, expectedStatusCode int, rule DetectionRule) (bool, error) {
+func (hs *HostScanner) scanEvent(event l9format.L9Event, uri string, expectedStatusCode int) (bool, error) {
 	finalUrl := event.Url() + uri
 	resp, err := hs.HttpClient.Get(finalUrl)
 	if err != nil {
@@ -80,7 +80,7 @@ func (hs *HostScanner) scanWithRule(event l9format.L9Event, uri string, expected
 	log.Printf("Scan results: [%d] %s", resp.StatusCode, finalUrl)
 	if resp.StatusCode != expectedStatusCode {
 		// Check rule-specific exclusions
-		for _, exception := range rule.ExceptionURLs {
+		for _, exception := range hs.Rule.ExceptionURLs {
 			if strings.Contains(uri, exception.Uri) && resp.StatusCode == exception.StatusCode {
 				return false, nil
 			}
@@ -92,7 +92,7 @@ func (hs *HostScanner) scanWithRule(event l9format.L9Event, uri string, expected
 		event.EventType = "leak"
 		event.EventSource = "GenericIOCScan"
 		event.EventPipeline = append(event.EventPipeline, event.EventSource)
-		event.Summary = fmt.Sprintf("%s abnormal reply:\n", rule.Name)
+		event.Summary = fmt.Sprintf("%s abnormal reply:\n", hs.Rule.Name)
 		event.Summary += fmt.Sprintf("Found %d instead of %d on %s", resp.StatusCode, expectedStatusCode, finalUrl)
 		event.Leak.Severity = "critical"
 		event.Leak.Dataset.Infected = true
@@ -107,18 +107,18 @@ func (hs *HostScanner) scanWithRule(event l9format.L9Event, uri string, expected
 
 func (hs *HostScanner) scanUrls(event l9format.L9Event) {
 	// Check if target matches this rule's fingerprint
-	if !hs.testSoftwareFingerprint(event, *hs.Rule) {
+	if !hs.testSoftwareFingerprint(event) {
 		return
 	}
 	// Get normal status code for non-existing files
-	normalStatusCode := hs.getNormalStatusCodeForRule(event, *hs.Rule)
+	normalStatusCode := hs.getNormalStatusCodeForRule(event)
 	if normalStatusCode == 0 {
 		return
 	}
 	// Scan IOCs for this rule
 	connErrorCount := 0
 	for _, uri := range hs.Rule.IOCs {
-		found, err := hs.scanWithRule(event, uri, normalStatusCode, *hs.Rule)
+		found, err := hs.scanEvent(event, uri, normalStatusCode)
 		// If found, no need to go deeper
 		if found {
 			return
--- cmd/HttpIOCScan/main.go
+++ cmd/HttpIOCScan/main.go
@@ -1,21 +1,66 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"github.com/LeakIX/l9format"
 	"github.com/alecthomas/kong"
 	"github.com/leakix/HttpIOCScan"
 	"log"
+	"net/url"
 	"os"
+	"strings"
 	"sync"
 	"time"
 )
 
 type CLI struct {
-	InputFile  string        `arg:"" name:"input" help:"JSON file containing targets to scan" type:"existingfile"`
+	InputFile  string        `arg:"" name:"input" help:"JSON file containing targets to scan OR text file with URLs (one per line)" type:"existingfile"`
 	ConfigFile string        `arg:"" name:"config" help:"JSON configuration file with detection rules" type:"existingfile"`
 	Routines   int           `short:"r" long:"routines" help:"Number of concurrent scanning routines" default:"1000"`
 	Delay      time.Duration `short:"d" long:"delay" help:"Base delay between requests (randomized +0-900ms)" default:"1s"`
+}
+
+// parseURL converts a URL string to an L9Event
+func parseURL(urlStr string) (l9format.L9Event, error) {
+	parsedURL, err := url.Parse(strings.TrimSpace(urlStr))
+	if err != nil {
+		return l9format.L9Event{}, err
+	}
+
+	// Default to HTTPS port if no port specified
+	port := parsedURL.Port()
+	if port == "" {
+		if parsedURL.Scheme == "http" {
+			port = "80"
+		} else {
+			port = "443"
+		}
+	}
+
+	host := parsedURL.Hostname()
+	
+	return l9format.L9Event{
+		Ip:   host, // Will be resolved during scanning
+		Port: port,
+		Host: host,
+	}, nil
+}
+
+// isJSONFormat checks if the first line of the file looks like JSON
+func isJSONFormat(filename string) (bool, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return false, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	if scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		return strings.HasPrefix(line, "{") && strings.Contains(line, "\""), nil
+	}
+	return false, scanner.Err()
 }
 
 func main() {
@@ -28,40 +73,67 @@ func main() {
 		log.Fatalf("Error loading config file: %v", err)
 	}
 
+	// Prepare host scanners
 	hostChannel := make(chan l9format.L9Event)
 	waitGroup := &sync.WaitGroup{}
-	StartHostScanners(waitGroup, cli.Routines, hostChannel, rule, cli.Delay)
-	hostListJson, err := os.Open(cli.InputFile)
-	if err != nil {
-		ctx.Fatalf("Error opening input file: %v", err)
-	}
-	defer hostListJson.Close()
-
-	jsonDecoder := json.NewDecoder(hostListJson)
-	for {
-		var event l9format.L9Event
-		err := jsonDecoder.Decode(&event)
-		if err != nil {
-			break
-		}
-		hostChannel <- event
-	}
-	close(hostChannel)
-	waitGroup.Wait()
-}
-
-func StartHostScanners(waitGroup *sync.WaitGroup, maxRoutines int, hostChan chan l9format.L9Event, rule *HttpIOCScan.DetectionRule, delay time.Duration) {
 	OutputEncoder := json.NewEncoder(os.Stdout)
-	HttpClient := HttpIOCScan.GetSaneHttpClient(maxRoutines)
-	for i := 0; i < maxRoutines; i++ {
+	HttpClient := HttpIOCScan.GetSaneHttpClient(cli.Routines)
+	// Start host scanners
+	for i := 0; i < cli.Routines; i++ {
 		hs := HttpIOCScan.HostScanner{
 			WaitGroup:     waitGroup,
-			HostChannel:   hostChan,
+			HostChannel:   hostChannel,
 			OutputEncoder: OutputEncoder,
 			HttpClient:    HttpClient,
 			Rule:          rule,
-			Delay:         delay,
+			Delay:         cli.Delay,
 		}
 		go hs.Start()
 	}
+	// Determine input format and load targets
+	isJSON, err := isJSONFormat(cli.InputFile)
+	if err != nil {
+		ctx.Fatalf("Error checking input file format: %v", err)
+	}
+
+	inputFile, err := os.Open(cli.InputFile)
+	if err != nil {
+		ctx.Fatalf("Error opening input file: %v", err)
+	}
+	defer inputFile.Close()
+
+	if isJSON {
+		// Handle JSON format (existing behavior)
+		jsonDecoder := json.NewDecoder(inputFile)
+		for {
+			var event l9format.L9Event
+			err := jsonDecoder.Decode(&event)
+			if err != nil {
+				break
+			}
+			hostChannel <- event
+		}
+	} else {
+		// Handle URL list format (new behavior)
+		scanner := bufio.NewScanner(inputFile)
+		for scanner.Scan() {
+			line := strings.TrimSpace(scanner.Text())
+			if !strings.HasPrefix(line, "http://") && !strings.HasPrefix(line, "https://") {
+				continue // Skip lines not starting with http:// or https://
+			}
+			
+			event, err := parseURL(line)
+			if err != nil {
+				log.Printf("Error parsing URL '%s': %v", line, err)
+				continue
+			}
+			hostChannel <- event
+		}
+		
+		if err := scanner.Err(); err != nil {
+			log.Printf("Error reading input file: %v", err)
+		}
+	}
+	close(hostChannel)
+	waitGroup.Wait()
 }
